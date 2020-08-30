938dac958c7f93cb097a00d415e4fb298013f877
Mason Jarr <rrajnosam@gmail.com>
2020-08-29 17:39:09 -0700

    fix bug where script skips ones that it cannot create a proper url for

--- main.py
+++ main.py
@@ -17,9 +17,10 @@ options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
-executable_path=os.environ.get("CHROMEDRIVER_PATH")
+executable_path = os.environ.get("CHROMEDRIVER_PATH")
 
-browser = webdriver.Chrome(options=options)
+browser = webdriver.Chrome(executable_path=executable_path, options=options)
+# browser = webdriver.Chrome()
 
 
 class Suggestion:
@@ -31,9 +32,11 @@ class Suggestion:
 
 
 try:
-    browser.get(f"https://www.showcasingcreators.com/suggest-channel/admin/show?admin={os.environ.get('ADMIN')}")    
+    browser.get(
+        f"https://www.showcasingcreators.com/suggest-channel/admin/show?admin={os.environ.get('ADMIN')}")
 
-    ids = browser.find_elements_by_css_selector("button.btn.btn-outline-danger")
+    ids = browser.find_elements_by_css_selector(
+        "button.btn.btn-outline-danger")
     urls = browser.find_elements_by_css_selector(".url")
     categories = browser.find_elements_by_css_selector(".category")
     tags = browser.find_elements_by_css_selector(".tags")
@@ -46,7 +49,7 @@ try:
     for eachIds in ids:
         # print(eachIds.get_attribute("id"))
         try:
-            idsArray.append(eachIds.get_attribute("id"))
+            idsArray.append(eachIds.get_attribute("id").strip())
         except Exception as e:
             print(e)
             continue
@@ -54,10 +57,12 @@ try:
     for eachUrl in urls:
         # print(eachUrl.text)
         try:
-            eachUrl = eachUrl.text.split("/")
-            eachUrl = "https://www.youtube.com/" + eachUrl[3] + "/" + eachUrl[4]
-            urlArray.append(eachUrl)
+            modUrl = eachUrl.text.strip().split("/")
+            modUrl = "https://www.youtube.com/" + \
+                modUrl[3] + "/" + modUrl[4]
+            urlArray.append(modUrl)
         except Exception as e:
+            urlArray.append(eachUrl.strip())
             print(e)
             continue
     for eachCategory in categories:
@@ -65,6 +70,7 @@ try:
         try:
             categoryArray.append(eachCategory.text)
         except Exception as e:
+            categoryArray.append("--")
             print(e)
             continue
     for eachTags in tags:
@@ -72,9 +78,10 @@ try:
         try:
             tagsArray.append(eachTags.text)
         except Exception as e:
+            tagsArray.append("--")
             print(e)
             continue
-    
+
     print(idsArray)
     print(urlArray)
     print(categoryArray)
@@ -84,7 +91,8 @@ try:
 
     for index, url in enumerate(urlArray):
         try:
-            item = Suggestion(url, categoryArray[index], tagsArray[index], idsArray[index])
+            item = Suggestion(
+                url, categoryArray[index], tagsArray[index], idsArray[index])
             suggestionObjects.append(item)
         except Exception as e:
             print(e)
@@ -92,75 +100,80 @@ try:
 
     for suggestion in suggestionObjects:
         try:
-            print("-----------------------------------------------------------------------")
+            print(
+                "-----------------------------------------------------------------------")
             print(suggestion.url, suggestion.category, suggestion.tags)
 
             browser.get(suggestion.url)
 
             channelName = browser.find_element_by_css_selector(
                 ".ytd-channel-name > #text").text
-            print(channelName)
+            print("name: ", channelName)
 
             image = browser.find_element_by_css_selector(
                 "#avatar>#img").get_attribute("src")
-            print(image)
+            print("image url: ", image)
 
             browser.get(suggestion.url+"/about")
             description = browser.find_element_by_css_selector(
                 "#description.style-scope.ytd-channel-about-metadata-renderer").text
-            print(description)
+            print("description: ", description)
 
-            browser.get(f"https://www.showcasingcreators.com/suggest-channel/admin?admin={os.environ.get('ADMIN')}")
+            browser.get(
+                f"https://www.showcasingcreators.com/suggest-channel/admin?admin={os.environ.get('ADMIN')}")
 
-            # time.sleep(3)
-            # print(browser.page_source)
-
-            print("input description")
-            inputDescription = browser.find_element_by_xpath("//*[@id='channelDescription']")
+            print("---------------submit channel-------------")
+            print("inputting description...")
+            inputDescription = browser.find_element_by_xpath(
+                "//*[@id='channelDescription']")
             inputDescription.clear()
             inputDescription.send_keys(description)
 
-            print("channel url")
+            print("inputting channel url...")
             inputUrl = browser.find_element_by_id("channelUrl")
             inputUrl.clear()
             inputUrl.send_keys(suggestion.url)
 
-            print("input name")
+            print("inputting name...")
             inputName = browser.find_element_by_id("channelName")
             inputName.clear()
             inputName.send_keys(channelName)
 
-            print("input image")
+            print("inputting image url...")
             inputImg = browser.find_element_by_id("channelImg")
             inputImg.clear()
             inputImg.send_keys(image)
 
-            
-            print("input category")
+            print("inputting category...")
             inputTags = browser.find_element_by_id("channelTags")
             inputTags.location_once_scrolled_into_view
             inputTags.clear()
             inputTags.send_keys(suggestion.category, ", ", suggestion.tags)
 
-            print("input password")
+            print("inputting password...")
             inputPassword = browser.find_element_by_id("admin")
             inputPassword.clear()
             inputPassword.send_keys(f"{os.environ.get('ADMIN')}")
 
             browser.find_element_by_id("saveChannel").click()
 
-            print("channel saved")
+            successMessage = browser.find_element_by_css_selector("body").text
 
-            browser.get(f"https://www.showcasingcreators.com/suggest-channel/admin/show?admin={os.environ.get('ADMIN')}")
+            if successMessage == "successfully saved to database":
+                print("channel saved")
+                browser.get(
+                    f"https://www.showcasingcreators.com/suggest-channel/admin/show?admin={os.environ.get('ADMIN')}")
 
-            # time.sleep(3)
-            print("deleting suggestion...")
-            browser.find_element_by_id("admin-password").send_keys(f"{os.environ.get('ADMIN')}")
-            browser.find_element_by_id(suggestion.id).click()
-            browser.find_element_by_id("admin-password").clear()
+                print("deleting suggestion...")
+                browser.find_element_by_id(
+                    "admin-password").send_keys(f"{os.environ.get('ADMIN')}")
+                browser.find_element_by_id(suggestion.id).click()
+                browser.find_element_by_id("admin-password").clear()
 
-            print("suggestion deleted")
-        
+                print("suggestion deleted")
+            else:
+                print("channel not saved")
+
         except Exception as e:
             print(e)
             continue
